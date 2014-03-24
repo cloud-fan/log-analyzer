a01633f4afceea0796924f734c72b3f21dd7b840
Wenchen Fan(Cloud) <cloud0fan@gmail.com>
2014-03-24 19:32:40 +0800

    add assembly plugin and change log dir

---
+++ assembly.sbt
@@ -0,0 +1,5 @@
+import AssemblyKeys._
+
+assemblySettings
+
+jarName := "log-analyzer.jar"
--- src/main/scala/cloud/fan/Main.scala
+++ src/main/scala/cloud/fan/Main.scala
@@ -4,6 +4,9 @@ import scala.collection.mutable.ListBuffer
 import scala.concurrent._
 import scala.concurrent.duration._
 import java.util.concurrent.Executors
+import java.nio.file.{Files, Paths}
+import java.text.SimpleDateFormat
+import java.util.Date
 
 /**
  * Created by cloud on 3/21/14.
@@ -13,33 +16,37 @@ object Main {
   val futures = ListBuffer.empty[Future[Unit]]
   implicit val threadPool = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(64))
 
-  def doWork(nodes: Array[String], groups: Array[String], nodeType: String, baseLogDir: String) {
+  def doWork(nodes: Array[String], groups: Array[String], nodeType: String, logDir: String) {
     ChartSender.sendNodes(nodeType, nodes)
     for(node <- nodes) {
       ChartSender.sendGroups(nodeType, node, groups)
       for (group <- groups) {
         futures += future {
-          analyzeLog(nodeType, node, baseLogDir, group)
+          analyzeLog(nodeType, node, logDir, group)
         }
       }
     }
   }
 
   def main(args: Array[String]) {
-    val baseLogDir = args(0)
-    val webPath = args(1)
+    val tag = args(0)
+    val dirName = s"$tag-${new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date)}"
+    val webPath = Paths.get(args(1), dirName).toAbsolutePath.toString
+    val logDir = Paths.get("/tmp", dirName).toAbsolutePath.toString
+    Files.createDirectory(Paths.get(logDir))
     val serverNames = args(2).split(",")
     val serverGroups = args(3).split(",")
-    doWork(serverNames, serverGroups, "server", baseLogDir)
+    doWork(serverNames, serverGroups, "server", logDir)
 
     if (args.length == 6) {
       val clientNames = args(4).split(",")
       val clientGroups = args(5).split(",")
-      doWork(clientNames, clientGroups, "client", baseLogDir)
+      doWork(clientNames, clientGroups, "client", logDir)
     }
 
     sys.addShutdownHook {
       ChartSender.finish(webPath)
+      Files.move(Paths.get(logDir), Paths.get(webPath, "logs"))
       System.out.close()
       System.err.close()
     }
