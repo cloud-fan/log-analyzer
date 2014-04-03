1694311c6bc82fa7609935537b86cc0c0c8873f0
Wenchen Fan(Cloud) <cloud0fan@gmail.com>
2014-04-03 15:27:16 +0800

    split top group into 2 charts

--- src/main/scala/cloud/fan/analyzeTopLog.scala
+++ src/main/scala/cloud/fan/analyzeTopLog.scala
@@ -13,12 +13,14 @@ object analyzeTopLog extends LogAnalyzer {
   def apply(nodeType: String, node: String, logDir: String, process: String) {
     val pid = Seq("ssh", node, "ps aux|grep "+process+"|grep -v grep|tail -n 1|awk '{print $2}'").!!.trim
     if (pid.matches("\\d+")) {
-      charts += new Chart("top", s"CPU and Memory utilization of $process", percentage, Array("cpu", "memory"))
+      charts += new Chart("cpu", s"CPU utilization of $process", percentage, Array("cpu"))
+      charts += new Chart("memory", s"memory utilization of $process", percentage, Array("memory"))
       analyzeLog.initCharts(nodeType, node, group, charts.toArray)
       val logIterator = analyzeLog.getLogContentIterator(command + pid, node, logDir)
       logIterator.map(_.trim).filter(_.matches("\\d+.+")).foreach {line =>
         val data = line.split("\\s+")
-        ChartSender.sendData(nodeType, node, group, charts.head.name, Array(data(8), data(9)))
+        ChartSender.sendData(nodeType, node, group, charts.head.name, Array(data(8)))
+        ChartSender.sendData(nodeType, node, group, charts.last.name, Array(data(9)))
       }
     } else {
       System.err.println(s"process $process can not found at remote server $node!")
