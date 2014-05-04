89acc94cf28bc169166291923d2369178dc92a0b
Wenchen Fan(Cloud) <cloud0fan@gmail.com>
2014-05-04 12:56:51 +0800

    fix bug in process finder

--- src/main/scala/cloud/fan/ProcessFinder.scala
+++ src/main/scala/cloud/fan/ProcessFinder.scala
@@ -7,25 +7,9 @@ import scala.collection.mutable
  */
 object ProcessFinder {
 
-  val cachedProcessId = mutable.HashMap.empty[String, Option[Array[String]]]
+  val cachedProcessId = mutable.HashMap.empty[String, Option[String]]
 
   def getProcessIds(processName: String, node: String) = {
-    if (cachedProcessId.contains(processName)) {
-      cachedProcessId(processName)
-    } else {
-      cachedProcessId.synchronized {
-        if (cachedProcessId.contains(processName)) {
-          cachedProcessId(processName)
-        } else {
-          val result = doWork(processName, node)
-          cachedProcessId += processName -> result
-          result
-        }
-      }
-    }
-  }
-
-  private def doWork(processName: String, node: String) = {
     val command = Seq(Seq("ps", "aux"), Seq("grep", processName), Seq("grep", "-v", "grep"), Seq("grep", "-v", "log-analyzer"), Seq("awk", "{print $2}"))
     val output = ShUtil.generatePipedCommand(command, node).!!
     if (output.length > 0) {
@@ -35,23 +19,40 @@ object ProcessFinder {
     }
   }
 
-  def getUniqueProcessId(processName: String, node: String, reTryCount: Int = 40): Option[String] = {
-    getProcessIds(processName, node) match {
-      case Some(pid) =>
-        if (pid.size == 1) {
-          Some(pid.head)
+  def getUniqueProcessId(processName: String, node: String): Option[String] = {
+
+    def doWork(reTryCount: Int = 40): Option[String] = {
+      getProcessIds(processName, node) match {
+        case Some(pid) =>
+          if (pid.size == 1) {
+            Some(pid.head)
+          } else {
+            System.err.println("non unique process name! pid are: " + pid.mkString(","))
+            None
+          }
+        case None =>
+          if (reTryCount > 0) {
+            Thread.sleep(2000)
+            doWork(reTryCount - 1)
+          } else {
+            System.err.println(s"process $processName can not found at remote server $node!")
+            None
+          }
+      }
+    }
+
+    if (cachedProcessId.contains(processName)) {
+      cachedProcessId(processName)
+    } else {
+      cachedProcessId.synchronized {
+        if (cachedProcessId.contains(processName)) {
+          cachedProcessId(processName)
         } else {
-          System.err.println("non unique process name! pid are: " + pid.mkString(","))
-          None
+          val result = doWork()
+          cachedProcessId += processName -> result
+          result
         }
-      case None =>
-        if (reTryCount > 0) {
-          Thread.sleep(2000)
-          getUniqueProcessId(processName, node, reTryCount - 1)
-        } else {
-          System.err.println(s"process $processName can not found at remote server $node!")
-          None
-        }
+      }
     }
   }
 }
